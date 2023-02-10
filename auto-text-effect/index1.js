78d13218c776b058884a7afb80e2ed20b203b3e4
Patrick <pb760@uowmail.edu.au>
2023-02-10 14:21:18 +1100

    html, css and javascript for auto-text-effect animation updated

--- auto-text-effect/index1.js
+++ auto-text-effect/index1.js
@@ -1,16 +1,14 @@
-// select the container
 const containerEl = document.querySelector(".container");
-// an array of careers
-const careers = ["Web Developer", "Youtuber", "Freelancer", "Instructor"];
-// set default value for career - Web Developer
+
+const careers = ["YouTuber", "Web Developer", "Freelancer", "Instructor"];
+
 let careerIndex = 0;
-// get the index of the characters in careers array
+
 let characterIndex = 0;
 
 updateText();
+
 function updateText() {
-  characterIndex++;
-  // manipulate the containerEl
   characterIndex++;
   containerEl.innerHTML = `
     <h1>I am ${careers[careerIndex].slice(0, 1) === "I" ? "an" : "a"} ${careers[
@@ -18,12 +16,11 @@ function updateText() {
   ].slice(0, characterIndex)}</h1>
     `;
 
-  // call the function more than once after a delay
   if (characterIndex === careers[careerIndex].length) {
     careerIndex++;
     characterIndex = 0;
   }
-  // reset the careerIndex to 0 so that the loop will continue
+
   if (careerIndex === careers.length) {
     careerIndex = 0;
   }
